fc418b06c05ecb2fd4e1d1661bd67f5a3339fd71
randhavevaibhav <randhavevaibhav3@gmail.com>
2025-11-23 19:34:46 +0530

    header styling

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,10 +1,30 @@
 import Link from "next/link";
+import { Logo } from "./logo";
+
+const routes = [
+  {
+    name: "Home",
+    path: "/",
+  },
+  {
+    name: "All Events",
+    path: "/events/all",
+  },
+];
 
 export const Header = () => {
   return (
-    <header>
-      <Link href={`/`}>Home</Link>
-      <Link href={`/events/all`}>All Events</Link>
+    <header className="flex justify-between items-center border-b border-white/10 h-14 px-3 sm:px-9">
+      <Logo />
+      <ul className="flex gap-x-6 text-sm">
+        {routes.map((route, idx) => {
+          return (
+            <li key={idx} className="text-white/50 hover:text-white transition">
+              <Link href={route.path}>{route.name}</Link>
+            </li>
+          );
+        })}
+      </ul>
     </header>
   );
 };
