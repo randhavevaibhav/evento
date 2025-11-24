924ec789e5b66bbec6a9dad9b1f7bc352e6465d6
randhavevaibhav <randhavevaibhav3@gmail.com>
2025-11-24 14:48:54 +0530

    styled scroll bar

--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -1,3 +1,9 @@
+import { H1 } from "@/components/H1";
+
 export default function EventsPage() {
-  return <div>Events page</div>;
+  return (
+    <main className="flex flex-col items-center py-24 px-[20px] min-h-[110vh]">
+      <H1>Events in Austin</H1>
+    </main>
+  );
 }
